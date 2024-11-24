18fab39b4361d07f002faeb59478a057a59c4794
EE <ezinercan@gmail.com>
2024-11-25 00:27:41 +0300

    webp

---
+++ teaching/2425-autumn/webp/files/w10_change_script.js
@@ -0,0 +1,10 @@
+function onClick() {
+    const element = document.querySelector('#catpicture');
+    element.src ='simba.png';
+}
+
+const button = document.querySelector('button');
+button.addEventListener('click', onClick);
+
+
+
---
+++ teaching/2425-autumn/webp/files/w10_script.js
@@ -0,0 +1,9 @@
+function onClick() {
+    console.log('Gönderildi');
+}
+
+const button = document.querySelector('button');
+button.addEventListener('click', onClick);
+
+
+
